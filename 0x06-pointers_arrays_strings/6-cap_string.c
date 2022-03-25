715299136a662afe29fda638be2f857c0b7d9ddd
Blessing <Adegboroblessing2@gmail.com>
2022-03-25 13:53:24 -0700

    Expect the best. prepare for the worst

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,16 +1,14 @@
 #include "main.h"
-
 /**
- * cap_string - capitalizes llwords of a string
+ * cap_string - capitalizes all words of a string
  * @s: input string
  *
  * Return: the pointer to dest
  */
-
 char *cap_string(char *s)
 {
 	int count = 0, i;
-	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125}
 
 	if (*(s + count) >= 97 && *(s + count) <= 122)
 		*(s + count) = *(s + count) - 32;
@@ -21,13 +19,12 @@ char *cap_string(char *s)
 		{
 			if (*(s + count) == sep_words[i])
 			{
-				if ((*(s + (count + 1)) >= 97 && (*s + (count + 1)) <= 122))
-						*(s + (count + 1)) = *(s + (count + 1)) - 32;
+				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+					*(s + (count + 1)) = *(s + (count + 1)) - 32;
 				break;
 			}
 		}
 		count++;
 	}
-	return (s);
-
+return (s);
 }

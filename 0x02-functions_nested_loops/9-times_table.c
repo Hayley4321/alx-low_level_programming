222f84f45b7d5886c31259d35affbfcbd93c981f
Blessing <Adegboroblessing2@gmail.com>
2022-03-27 10:36:54 -0700

    learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,13 +5,15 @@
 void times_table(void)
 {
 	int a;
-int b;
-for (a = 0; a <= 9; a++)
+	int b;
+
+	for (a = 0; a <= 9; a++)
 	{
 		for (b = 0; b <= 9; b++)
 		{
 			int ans = a * b;
-if (ans < 10)
+
+			if (ans < 10)
 				_putchar((ans % 10) + '0');
 			if (ans >= 10)
 			{

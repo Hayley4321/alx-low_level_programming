fd1c4a54e1a531c97300f09987d40647e95eea54
Blessing <Adegboroblessing2@gmail.com>
2022-03-27 00:19:28 -0700

    print lowcase alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,14 @@
-#include "main. H"
+#include "main.h"
 /**
 *print _alphabet - program that prints the alphabet
-*Return :Always 0
-* /
+*Return: Always 0
+*/
 int print_alphabet(void)
 {
-      char letter;
-       for(letter = 'a' ; letter <= 'z' ; letter++)
-_putchar(letter) ;
+	char letter;
 
-_putchar('\n') ;
-
-return (0);
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
+	return (0);
 }

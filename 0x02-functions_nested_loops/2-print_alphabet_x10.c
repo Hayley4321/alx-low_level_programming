472f1d3f5619e236a9cf313d5bf9a09e78fd9422
Blessing <Adegboroblessing2@gmail.com>
2022-03-27 10:10:54 -0700

    print alphabets

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,22 +5,14 @@
 *
 */
 
-void print_alphabet_x10(void)
+int print_alphabet_x10(void)
 {
-<<<<<<< HEAD
        int a =0;
 
        while (a <= 9)
 	{
 		char letter;
 
-=======
-	int a = 0;
-	
-	while (a <= 9)
-	{
-		char letter;
->>>>>>> 90bf25d4ef27e48364c54da56e845fb7000f4c5d
 		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
 		_putchar('\n');
